78a8e51aaa6fa897676013b872367f440e7845a6
marsholirs <clara.wroe@gmail.com>
2025-04-02 14:43:40 +0000

    basic tarot reader it does one card i

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,51 @@
 import streamlit as st
+import pandas as pd
+import random
+colnames=['cards','meanings','reverse_meanings']
+cards=pd.read_csv("tarot.csv",names=colnames)
+options = ["One card", "Three cards"]
+selection = st.pills("Type of reading", options, selection_mode="single")
+repeat=0
+if selection=="One card":
+    if st.button("Get tarot reading"):
+        num=random.randint(0,21)
+        rev=random.randint(0,1)
+        card=cards.at[num,'cards']
+        mean=cards.at[num,'meanings']
+        rev_mean=cards.at[num,'reverse_meanings']
+        st.title(card)
+        if rev==1:
+            st.write("This card indicates",mean,"in your life.")
+        else:
+            st.write("This card in reverse indictates",rev_mean,"in your life.")
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+if selection=="Three cards":
+    if st.button("Get tarot reading"):
+        num1=random.randint(0,21)
+        num2=random.randint(0,21)
+        num3=random.randint(0,21)
+        while num1==num2 or num2==num3:
+            num2=random.randint(0,21)
+        while num1==num3 or num2==num3:
+            num3=random.randint(0,21)
+        rev=random.randint(0,1)
+        card=cards.at[num1,'cards']
+        mean=cards.at[num1,'meanings']
+        rev_mean=cards.at[num1,'reverse_meanings']
+        st.title(card)
+        if rev==1:
+            st.write("This card indicates",mean,"in your past.")
+        else:
+            st.write("This card in reverse indictates",rev_mean,"in your past.")
+        if st.button("See next card"):
+            st.title("hello")
+            rev=rev=random.randint(0,1)
+            card2=cards.at[num2,'cards']
+            mean2=cards.at[num2,'meanings']
+            rev_mean2=cards.at[num2,'reverse_meanings']
+            st.title(card2)
+            if rev==1:
+                st.write("This card indicates",mean2,"in your present.")
+            else:
+                st.write("This card in reverse indictates",rev_mean2,"in your present.")
+            
