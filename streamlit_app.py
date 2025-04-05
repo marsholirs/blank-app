618566a992f6d08776328e0cea258ed61b2dd90d
marsholirs <clara.wroe@gmail.com>
2025-04-05 16:51:30 +0000

    fixed everything i broke when i added colums

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,19 @@ st.markdown("""
 col1, col2, col3 = st.columns(3, vertical_alignment="bottom")
 colnames=['cards','meanings','reverse_meanings']
 cards=pd.read_csv("tarot.csv",names=colnames)
+
+num=random.randint(0,21)
+num2=random.randint(0,21)
+num3=random.randint(0,21)
+while num==num2 or num2==num3:
+    num2=random.randint(0,21)
+while num==num3 or num2==num3:
+    num3=random.randint(0,21)
 with col2:
     options = ["One card", "Three cards"]
     selection = st.pills("Type of reading", options, selection_mode="single")
 
-num=random.randint(0,21)
+
 if num==0:
     file_ = open("the fool.gif", "rb")
 elif num==1:
@@ -71,8 +79,6 @@ with col2:
     if selection=="One card":
         if st.button("Get tarot reading"):
             st.divider()
-
-
             rev=random.randint(0,1)
             card=cards.at[num,'cards']
             mean=cards.at[num,'meanings']
@@ -96,17 +102,7 @@ if selection=="Three cards":
     with col2:
         if st.button("Get tarot reading"):
             st.divider()
-            num2=random.randint(0,21)
-            num3=random.randint(0,21)
-            while num==num2 or num2==num3:
-                num2=random.randint(0,21)
-            while num==num3 or num2==num3:
-                num3=random.randint(0,21)
             st.write(num,num2,num3)
-            
-            card=cards.at[num,'cards']
-            mean=cards.at[num,'meanings']
-            rev_mean=cards.at[num,'reverse_meanings']
         with col1:
             card=cards.at[num,'cards']
             mean=cards.at[num,'meanings']
@@ -128,78 +124,77 @@ if selection=="Three cards":
                 f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
                 unsafe_allow_html=True,
             )
-        num2=random.randint(0,21)
-        rev=rev=random.randint(0,1)
-        card2=cards.at[num2,'cards']
-        mean2=cards.at[num2,'meanings']
-        rev_mean2=cards.at[num2,'reverse_meanings']
-        st.title("Your Present")
-        if rev==1:
-            st.write(card2)
-            st.write("this card indicates",mean2,"in your present.")
-        else:
-            st.write(card2,"reversed")
-            st.write("this card in reverse indictates",rev_mean2,"in your present.")
-        
-        if num2==0:
-            file_ = open("the fool.gif", "rb")
-        elif num2==1:
-            file_ = open("the magician .gif", "rb")
-        elif num2==2:
-            file_ = open("the high priestess .gif", "rb")
-        elif num2==3:
-            file_ = open("the empress.gif", "rb")
-        elif num2==4:
-            file_ = open("the emperor.gif", "rb")
-        elif num2==5:
-            file_ = open("the hierophant.gif", "rb")
-        elif num2==6:
-            file_ = open("the lovers.gif", "rb")
-        elif num2==7:
-            file_ = open("the chariot.gif", "rb")
-        elif num2==8:
-            file_ = open("strength.gif", "rb")
-        elif num2==9:
-            file_ = open("the hermit.gif", "rb")
-        elif num2==10:
-            file_ = open("the wheel of fate.gif", "rb")
-        elif num2==11:
-            file_ = open("justice.gif", "rb")       
-        elif num2==12:
-            file_ = open("the hanged man.gif", "rb")
-        elif num2==13:
-            file_ = open("death.gif", "rb")
-        elif num2==14:
-            file_ = open("temperance .gif", "rb")
-        elif num2==15:
-            file_ = open("the devil.gif", "rb")
-        elif num2==16:
-            file_ = open("the tower.gif", "rb")
-        elif num2==17:
-            file_ = open("the star.gif", "rb")
-        elif num2==18:
-            file_ = open("the moon.gif", "rb")
-        elif num2==19:
-            file_ = open("the sun.gif", "rb")
-        elif num2==20:
-            file_ = open("judgment .gif", "rb")
-        elif num2==21:
-            file_ = open("the world.gif", "rb")
+        with col2:
+            rev=random.randint(0,1)
+            card2=cards.at[num2,'cards']
+            mean2=cards.at[num2,'meanings']
+            rev_mean2=cards.at[num2,'reverse_meanings']
+            st.title("Your Present")
+            if rev==1:
+                st.write(card2)
+                st.write("this card indicates",mean2,"in your present.")
+            else:
+                st.write(card2,"reversed")
+                st.write("this card in reverse indictates",rev_mean2,"in your present.")
+            
+            if num2==0:
+                file_ = open("the fool.gif", "rb")
+            elif num2==1:
+                file_ = open("the magician .gif", "rb")
+            elif num2==2:
+                file_ = open("the high priestess .gif", "rb")
+            elif num2==3:
+                file_ = open("the empress.gif", "rb")
+            elif num2==4:
+                file_ = open("the emperor.gif", "rb")
+            elif num2==5:
+                file_ = open("the hierophant.gif", "rb")
+            elif num2==6:
+                file_ = open("the lovers.gif", "rb")
+            elif num2==7:
+                file_ = open("the chariot.gif", "rb")
+            elif num2==8:
+                file_ = open("strength.gif", "rb")
+            elif num2==9:
+                file_ = open("the hermit.gif", "rb")
+            elif num2==10:
+                file_ = open("the wheel of fate.gif", "rb")
+            elif num2==11:
+                file_ = open("justice.gif", "rb")       
+            elif num2==12:
+                file_ = open("the hanged man.gif", "rb")
+            elif num2==13:
+                file_ = open("death.gif", "rb")
+            elif num2==14:
+                file_ = open("temperance .gif", "rb")
+            elif num2==15:
+                file_ = open("the devil.gif", "rb")
+            elif num2==16:
+                file_ = open("the tower.gif", "rb")
+            elif num2==17:
+                file_ = open("the star.gif", "rb")
+            elif num2==18:
+                file_ = open("the moon.gif", "rb")
+            elif num2==19:
+                file_ = open("the sun.gif", "rb")
+            elif num2==20:
+                file_ = open("judgment .gif", "rb")
+            elif num2==21:
+                file_ = open("the world.gif", "rb")
 
-        contents = file_.read()
-        data_url = base64.b64encode(contents).decode("utf-8")
-        file_.close()
+            contents = file_.read()
+            data_url = base64.b64encode(contents).decode("utf-8")
+            file_.close()
 
-        st.markdown(
-            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-            unsafe_allow_html=True,
-        )
-        num3=random.randint(0,21)
-        rev=random.randint(0,1)
-        card3=cards.at[num3,'cards']
-        mean3=cards.at[num3,'meanings']
-        rev_mean3=cards.at[num3,'reverse_meanings']
+            st.markdown(
+                f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+                unsafe_allow_html=True,
+            )
         with col3:
+            rev=random.randint(0,1)
+            card3=cards.at[num3,'cards']
+            mean3=cards.at[num3,'meanings']
+            rev_mean3=cards.at[num3,'reverse_meanings']
             st.title("Your future")
             if rev==1:
                 st.write(card3.capitalize())
